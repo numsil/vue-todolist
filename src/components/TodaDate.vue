98eff5893f205090f654627e350f82801b6dd091
Jun <numsil8567@gmail.com>
2023-02-20 21:51:02 +0900

    feat : header 수정 및 dust date 작업

--- src/components/TodaDate.vue
+++ src/components/TodaDate.vue
@@ -4,8 +4,15 @@
       <div class="current-date">{{ currentDate }}</div>
       <div class="current-time">{{ currentTime }}</div>
     </div>
-    <div class="current-dust" v-for="item in dustData.dust" :key="item.id">
-      {{ item.stationName }}
+    <div
+      class="current-dust"
+      v-for="item in dustData.dust"
+      :key="item.stationName"
+    >
+      <p>측정소: {{ item.stationName }}</p>
+      <p>미세먼지 농도: {{ item.pm10Value }}</p>
+      <p>초미세먼지 농도: {{ item.pm25Value }}</p>
+      <p>측정일시: {{ item.dataTime }}</p>
     </div>
   </div>
 </template>
@@ -20,6 +27,8 @@ const currentTime = ref("");
 const dustData = reactive({
   dust: [] as any[],
 });
+
+// console.log("dust", dustData);
 
 const API_KEY =
   "UeXBqyTjPcS1Ksf%2FLPNCFNbjqK55nMckPrhG1bSNPYdPwas%2Fu5u9TztERQlljzW9alMlWEC1KzNvCoNAbGPfnQ%3D%3D";
@@ -38,6 +47,43 @@ const updateDateTime = () => {
   currentTime.value = now.toLocaleTimeString().slice(0, -3);
   // console.log("date", currentDate.value);
 };
+
+// var xhr = new XMLHttpRequest();
+// console.log("dust", xhr);
+// var url =
+//   "http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst"; /*URL*/
+// var queryParams =
+//   "?" + encodeURIComponent("serviceKey") + "=" + "서비스키"; /*Service Key*/
+// queryParams +=
+//   "&" + encodeURIComponent("returnType") + "=" + encodeURIComponent("xml"); /**/
+// queryParams +=
+//   "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("100"); /**/
+// queryParams +=
+//   "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /**/
+// queryParams +=
+//   "&" + encodeURIComponent("itemCode") + "=" + encodeURIComponent("PM10"); /**/
+// queryParams +=
+//   "&" + encodeURIComponent("dataGubun") + "=" + encodeURIComponent("HOUR"); /**/
+// queryParams +=
+//   "&" +
+//   encodeURIComponent("searchCondition") +
+//   "=" +
+//   encodeURIComponent("MONTH"); /**/
+// xhr.open("GET", url + queryParams);
+// xhr.onreadystatechange = function () {
+//   if (this.readyState == 4) {
+//     alert(
+//       "Status: " +
+//         this.status +
+//         "nHeaders: " +
+//         JSON.stringify(this.getAllResponseHeaders()) +
+//         "nBody: " +
+//         this.responseText
+//     );
+//   }
+// };
+
+// xhr.send("");
 
 onMounted(() => {
   getData();
