481704deb3de13625eac4d739c34a89dfcd078a8
Jun <numsil8567@gmail.com>
2023-02-26 22:40:21 +0900

    feat: modal page 생성

--- src/components/common/Modal.vue
+++ src/components/common/Modal.vue
@@ -1,5 +1,24 @@
 <template>
-  <div></div>
+  <div class="black-bg">
+    <div class="white-bg">
+      <h4>상세</h4>
+      <p>상세 내용</p>
+    </div>
+  </div>
 </template>
 <script setup lang="ts"></script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+</style>
